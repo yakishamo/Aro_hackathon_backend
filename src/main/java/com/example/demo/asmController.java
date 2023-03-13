ea1c85c7799b44ccd7db1592f8a6214e7a82eb52
guchi5 <zero19390901@gmail.com>
2023-03-13 08:15:20 +0000

    new commit

---
+++ src/main/java/com/example/demo/asmController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class asmController {
+    @RequestMapping("/asm/{mnemonic}")
+    public Assembly read(@PathVariable String mnemonic){
+        Assembly asm = new Assembly();
+        if(mnemonic.equals("") || mnemonic == null){
+            throw  new ResponseStatusException(HttpStatus.NOT_FOUND, "Not Found"); 
+        }
+        asm.setMnemonic(mnemonic);
+        String[] terms = asm.getMnemonic().split("_");
+        asm.setTerms(terms);
+        return asm;
+    }
+
+}
