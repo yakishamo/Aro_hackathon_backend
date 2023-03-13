f750639bbfedc9ad1d9d498f301aa316dd8b70e3
guchi5 <zero19390901@gmail.com>
2023-03-13 11:15:43 +0000

    add Calculator and Result

---
+++ src/main/java/com/example/demo/Calculater.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class Calculater {
+      
+    Calculater(CPU cpu, Memory memory, Assembly asm){
+        
+    }
+
+
+    void run(){
+        
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/Result.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class Result {
+
+    private String[] register;
+    private String[] memory;
+
+    public void setRegister(){
+        
+    }
+
+    public void setMemory(){
+
+    }
+
+    
+}
