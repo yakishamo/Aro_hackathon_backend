454af6f116af912774b4ed7e694d27a3fb2d5f48
guchi5 <zero19390901@gmail.com>
2023-03-13 08:25:48 +0000

    change split

--- src/main/java/com/example/demo/asmController.java
+++ src/main/java/com/example/demo/asmController.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,7 +15,7 @@ public class asmController {
             throw  new ResponseStatusException(HttpStatus.NOT_FOUND, "Not Found"); 
         }
         asm.setMnemonic(mnemonic);
-        String[] terms = asm.getMnemonic().split("_");
+        String[] terms = asm.getMnemonic().split(", ");
         asm.setTerms(terms);
         return asm;
     }
