16be05c89221e65e3237bb0821a4677bd957ff46
yakishamo <82935423+yakishamo@users.noreply.github.com>
2023-03-14 14:37:14 +0900

    fix controller #2

--- src/main/java/com/example/demo/asmController.java
+++ src/main/java/com/example/demo/asmController.java
@@ -6,19 +6,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import java.util.*;
 
 import com.example.demo.models.CPU;
 
 @RestController
 public class asmController {
-    @RequestMapping("/asm/")
+    @RequestMapping("/asm")
     public Result read(@RequestBody Assembly asm){
         
         if(asm.equals("") || asm == null){
-            throw  new ResponseStatusException(HttpStatus.NOT_FOUND, "Not Found"); 
+            throw  new ResponseStatusException(HttpStatus.NOT_FOUND, "Not Found");
         }
         String[] terms = asm.getMnemonic().split("[, ]");
-        asm.setTerms(terms);
+        asm.setTerms(Arrays.copyOfRange(terms, 1, terms.length));
+				asm.setMnemonic(terms[0]);
         CPU cpu = new CPU();
 				Result r = new Result();
         Calculator c = new Calculator(cpu, asm);
